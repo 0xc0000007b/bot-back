920217a1f317efecbd770dad3a06adcd9f1c38b4
0xc0000007b <rammleid123@gmail.com>
2023-05-24 23:41:55 +0300

    .

--- app.js
+++ app.js
@@ -22,7 +22,6 @@ const token = process.env.TOKEN;
 const bot = new Bot(token, { polling: true });
 const webApp = 'https://web-tg-app.netlify.app';
 const webAppForm = 'https://web-tg-app.netlify.app/form';
-let pizzaArray = [];
 const app = express();
 app.use(cors());
 app.use(express.json());
@@ -77,8 +76,7 @@ app.post('/web-data', (req, res) => __awaiter(void 0, void 0, void 0, function* 
                 title: 'Успешная покупка',
                 input_message_content: {
                     message_text: ` Поздравляю с покупкой, вы приобрели товар на сумму ${totalPrice}\nВаши покупки:\n${orders.map((item) => {
-                        pizzaArray.push(item);
-                        return `\n🍕  ${item.pizza.map((p) => `🍕 ${p.type}`)}`;
+                        return `\n🍕  ${item.type}`;
                     })}, \n\nчтобы указать данные и узнать через сколько прибудет ваш заказ,нажмите на кнопку с изображением 4 точек внизу в поле ввода сообщения и  нажмите на кнопку "заполнить форму"`,
                 },
                 reply_markup: {
@@ -98,14 +96,14 @@ app.post('/web-data', (req, res) => __awaiter(void 0, void 0, void 0, function* 
                 },
             });
         }
-        return res.status(200).json(pizzaArray);
+        return res.status(200).json();
     }
     catch (e) {
         return res.status(500).json({ error: 'nothing send' });
     }
 }));
 app.get('/pizza', (req, res) => __awaiter(void 0, void 0, void 0, function* () {
-    res.status(200).send(pizzaArray);
+    res.status(200).send();
 }));
 app.listen(8080, () => console.log(`server started on address http://localhost:8080`));
 const calcTime = (address) => __awaiter(void 0, void 0, void 0, function* () {
