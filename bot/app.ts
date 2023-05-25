70ddbac81c53f52eeda3a05e83a5c8eb9bc45ada
0xc0000007b <rammleid123@gmail.com>
2023-05-25 21:50:45 +0300

    .

--- bot/app.ts
+++ bot/app.ts
@@ -150,7 +150,7 @@ app.post('/web-data', async (req, res) => {
 app.get('/pizz', async (res: Response, req: Request) => {
   const pizzas = await Pizza.getRepository().find();
   console.log(pizzas)
-  return res.status(200).json(pizzas)
+  return pizzas
 });
 app.listen(8080, () =>
   console.log(`server started on address http://localhost:8080`)
