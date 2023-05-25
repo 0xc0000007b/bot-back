9c9c9ca05ba2fb0c7083b06ac0e5583df8baa5a7
0xc0000007b <rammleid123@gmail.com>
2023-05-25 16:29:19 +0300

    .

--- app.js
+++ app.js
@@ -105,7 +105,7 @@ app.post('/web-data', (req, res) => __awaiter(void 0, void 0, void 0, function* 
                 },
             });
         }
-        return res.status(200).json(...pizzaArray);
+        return res.status(200).json(...pizzas);
     }
     catch (e) {
         return res.status(500).json({ error: 'nothing send' });
