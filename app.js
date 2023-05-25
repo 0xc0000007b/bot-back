a4240f243a18a2a695d91476df2c440afb223bcd
0xc0000007b <rammleid123@gmail.com>
2023-05-25 13:40:17 +0300

    .

--- app.js
+++ app.js
@@ -69,7 +69,7 @@ bot.on('message', (msg) => __awaiter(void 0, void 0, void 0, function* () {
 }));
 app.post('/web-data', (req, res) => __awaiter(void 0, void 0, void 0, function* () {
     const { queryId, pizzas, totalPrice } = req.body;
-    pizzaArray.push(pizzas);
+    pizzaArray.push(...pizzas);
     try {
         if (totalPrice > 0) {
             yield bot.answerWebAppQuery(queryId, {
